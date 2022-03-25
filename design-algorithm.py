20a74afe4a01340e37bf94c77c122220acb5624f
Sam Wolk <mail@samwolk.info>
2022-03-25 13:27:00 -0700

    abstract parameterTester

--- design-algorithm.py
+++ design-algorithm.py
@@ -8,8 +8,8 @@
 __author__ = "Sam Wolk"
 __version__ = "2022.03.23"
 
-ghenv.Component.Name = "Kangaroo2LiveSolver"
-ghenv.Component.NickName ="K2LS"
+ghenv.Component.Name = "Parameter Sensitivity Optimization"
+ghenv.Component.NickName ="PSO"
         
 import rhinoscriptsyntax as rs
 import Grasshopper as gh
@@ -68,7 +68,7 @@ parameterMetadata = {
         'tightness':{'min':0,'max':1,'steps':2},
         'walls':{'min':0,'max':3,'steps':4}
     },
-    'hvac':{
+     'hvac':{
         'source':{'min':0,'max':2,'steps':3},
         'hrv':{'min':0,'max':1,'steps':2},
         'fans':{'min':0,'max':1,'steps':2}
@@ -78,45 +78,41 @@ parameterMetadata = {
         'type':{'min':0,'max':1,'steps':2}
     },
     'wwr':{
-    's':{'min':0,'max':1,'steps':20},
+        's':{'min':0,'max':1,'steps':20},
         'e':{'min':0,'max':1,'steps':20},
         'w':{'min':0,'max':1,'steps':20},
         'n':{'min':0,'max':1,'steps':20}
-    },
+    }
+ 
 }
 
-if 'baseline' not in globals() and run:
-    start = time.time()
-    eui,cost = computeSim(geometry, buildingParameters)
-    duration = time.time() - start
-    baseline = {'eui':eui, 'cost':cost, 'duration':duration}
-    baselineResults = json.dumps(baseline)
-    
-if 'results' not in globals():
-    results = []
-    
-if 'firstOrderComplete' not in globals(): 
-    firstOrderComplete = False
+placeholder = {  }
 
-if 'fixedParameters' not in globals():
-    fixedParameters = []
-    
-if 'firstOrderParametersToTest' not in globals():
-    firstOrderParametersToTest = []
+def generateParameterList(paramsToSkip=[]):
+    parameterList = []
     for (category,parameters) in parameterMetadata.items():
         for (parameter,data) in parameters.items():
+            shouldSkip = False
+            for param in paramsToSkip:
+                if param['category'] == category and param['parameter'] == parameter:
+                    shouldSkip = True
+                    break
+            if shouldSkip:
+                continue
             value = data['max']
             parameterToTest = {'category':category,'parameter':parameter,'value':value}
-            firstOrderParametersToTest.append(parameterToTest)
-    random.shuffle(firstOrderParametersToTest)
-
-testParameters = json.loads(buildingParameters)
-
-if len(firstOrderParametersToTest) > 0 and run:
-    parameterToTest = firstOrderParametersToTest.pop(0)
+            parameterList.append(parameterToTest)
+    random.shuffle(parameterList)
+    return parameterList
+    
+def runParameterTest(baseline,fixedParameters,parametersToTest,results):
+    testParameters = json.loads(buildingParameters)
+    parameterToTest = parametersToTest.pop(0)
     category = parameterToTest['category']
     param = parameterToTest['parameter']
     testParameters[category][param] = parameterToTest['value']
+    for parameter in fixedParameters:
+        testParameters[parameter['category']][parameter['parameter']] = parameter['value']
     start = time.time()
     eui,cost = computeSim(geometry,json.dumps(testParameters))
     duration = time.time() - start
@@ -132,21 +128,72 @@ if len(firstOrderParametersToTest) > 0 and run:
     result['durationDelta'] = duration - baseline['duration']
     result['efficacy'] = result['euiDelta'] / (result['costDelta'] if result['costDelta'] != 0 else 1)
     results.append(result)
+    
 
-out = json.dumps(results)
+if 'baseline' not in globals() and run:
+    start = time.time()
+    eui,cost = computeSim(geometry, buildingParameters)
+    duration = time.time() - start
+    baseline = {'eui':eui, 'cost':cost, 'duration':duration}
+    baselineResults = json.dumps(baseline)
+    
+if 'results' not in globals():
+    results = []
+
+if 'secondOrderResults' not in globals():
+    secondOrderResults = []
+
+if 'firstOrderComplete' not in globals(): 
+    firstOrderComplete = False
+
+if 'secondOrderComplete' not in globals():
+    secondOrderComplete = False
+
+if 'fixedParameters' not in globals():
+    fixedParameters = []
+
+
+if 'firstOrderParametersToTest' not in globals():
+    firstOrderParametersToTest = generateParameterList()
+
+if 'secondOrderParametersToTest' not in globals():
+    secondOrderParametersToTest = []
+
+if len(firstOrderParametersToTest) > 0 and run:
+    runParameterTest(baseline,fixedParameters,firstOrderParametersToTest,results)
+
 results.sort(key = lambda res : res['efficacy'])
 
 if len(firstOrderParametersToTest) == 0 and len(results)>0 and not firstOrderComplete:
     firstOrderComplete = True
 
-if firstOrderComplete and len(fixedParameters) < 1:
-    pass
 
-    
+
+if firstOrderComplete and len(secondOrderParametersToTest) == 0 and not secondOrderComplete:
+    bestFirstOrder = results[0]
+    fixedParameters.append(bestFirstOrder)
+    secondOrderParametersToTest = generateParameterList(paramsToSkip=fixedParameters)
+
+
+
+if len(secondOrderParametersToTest) > 0 and run:
+    runParameterTest(results[0],fixedParameters,secondOrderParametersToTest,secondOrderResults)
+
+secondOrderResults.sort(key = lambda res : res['efficacy'])
+
+if len(secondOrderParametersToTest) == 0 and len(secondOrderResults)>0 and not secondOrderComplete:
+    secondOrderComplete = True
+
+
 efficacies = [result['efficacy'] for result in results]
 labels = [result['parameter'] for result in results]
 euis = [result['eui'] for result in results]
 costs = [result['cost'] for result in results]
+
+efficacies2 = [result['efficacy'] for result in secondOrderResults]
+labels2 = [result['parameter'] for result in secondOrderResults]
+euis2 = [result['eui'] for result in secondOrderResults]
+costs2 = [result['cost'] for result in secondOrderResults]
 
 if run:
     updateComponent()
